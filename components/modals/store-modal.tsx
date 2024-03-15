a7a876122f4720d8972137ed61cc942795976f17
Ryan A. Hunt <ryanahunt@outlook.com>
2024-03-14 21:29:25 -0500

    initialize database, setup api routes

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,6 +6,12 @@ import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -14,9 +20,29 @@ const formSchema = z.object({
 export const StoreModal = () => {
     const storeModal = useStoreModal();
 
+    const [loading, setLoading] = useState(false);
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-    })
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try{
+            setLoading(true);
+
+            const response = await axios.post('/api/stores', values);
+
+            toast.success("Store created. ")
+
+        } catch (error) {
+            toast.error("Something went wrong. ");
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
     <Modal
@@ -25,6 +51,34 @@ export const StoreModal = () => {
     isOpen={storeModal.isOpen}
     onClose={storeModal.onClose}
     >
-        Future Create Store Form
+        <div>
+            <div className="space-y-4 py-2 pb-4">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                        control={form.control}
+                        name= "name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input disabled={loading} placeholder="Store Name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+
+                        )}/>
+                        <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                            <Button disabled={loading} variant={"outline"}
+                            onClick={storeModal.onClose}
+                            >Cancel
+                            </Button>
+                            <Button disabled={loading} type="submit">Continue</Button>
+                        </div>
+                            
+                    </form>
+                </Form>
+            </div>
+        </div>
     </Modal>
 )}
---
+++ lib/prismadb.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined
+}
+
+const prismadb = globalThis.prisma || new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL // Use the DATABASE_URL environment variable
+        }
+    }
+});
+
+if(process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
