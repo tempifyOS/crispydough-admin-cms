d9f6a8e6d5bae2dfcbde7ac1f7afe79f15de04c6
Ryan A. Hunt <ryanahunt@outlook.com>
2024-04-01 15:09:18 -0500

    feat: stripe dashboard button

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -5,6 +5,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 
 import { columns, OrderColumn } from "./columns";
+import { Button } from "@/components/ui/button";
 
 interface OrderClientProps {
   data: OrderColumn[];
@@ -13,11 +14,15 @@ interface OrderClientProps {
 export const OrderClient: React.FC<OrderClientProps> = ({
   data
 }) => {
+  const redirectToStripe = () => {
+    window.open('https://dashboard.stripe.com/payments');
+  };
   return (
     <>
-      <Heading title={`Orders (${data.length})`} description="Manage orders for your store" />
+      <Heading title={`Orders (${data.length})`} description="View orders for your store" />
       <Separator />
-      <DataTable searchKey="products" columns={columns} data={data} />
+      <Button onClick={redirectToStripe}>View Stripe Order Information</Button>
+      <DataTable searchKey={"products"} columns={columns} data={data} />
     </>
   );
 };
